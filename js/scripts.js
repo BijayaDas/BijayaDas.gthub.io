7731e600ea486df5b85db5819495bf2c79852aeb
BijayaDas <bkdthenovelist@gmail.com>
2016-06-11 23:26:18 +0530

    after git rebase and all

--- js/scripts.js
+++ js/scripts.js
@@ -270,5 +270,5 @@
     //$('#parallax-2').parallax("30%", 0.1);
     /*add as necessary*/
   }  
-
+console.log("%cDon’t worry about failure; you only have to be right once.", "text-shadow: 0 1px 0 #ccc,0 2px 0 #c9c9c9,0 3px 0 #bbb,0 4px 0 #b9b9b9,0 5px 0 #aaa,0 6px 1px rgba(0,0,0,.1),0 0 5px rgba(0,0,0,.1),0 1px 3px rgba(0,0,0,.3),0 3px 5px rgba(0,0,0,.2),0 5px 10px rgba(0,0,0,.25),0 10px 10px rgba(0,0,0,.2),0 20px 20px rgba(0,0,0,.15); color: red; font-size: 44px; font-weight: bolder");
 })(jQuery);
