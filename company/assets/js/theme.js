6f3939a569131069d63c31c8fe1d4f58c689fff2
= <bkdas@teamdrg.com>
2018-04-02 16:18:30 +0530

    email setup

--- company/assets/js/theme.js
+++ company/assets/js/theme.js
@@ -689,7 +689,7 @@ $(function() {
     var th = $(this);
     $.ajax({
       type: "POST",
-      url: "mail.php", //Change (mail.php path)
+      url: "//formspree.io/cygnini.tech@gmail.com", //Change (mail.php path)
       data: th.serialize()
     }).done(function() {
       alert("Thank you. Your message has been sent!");
